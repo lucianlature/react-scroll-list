82286ad9fbb7d18407d9a857be97f3c1bffaa09c
lucianlature <lucian.lature@gmail.com>
2015-05-18 17:20:01 +0100

    Added a simple reactified app.

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lucianl on 18/05/15.
+ */
+(function() {
+  'use strict';
+
+  var ScrollList = require('./react-scroll-list.es6');
+
+  React.render(<StrollList />, document.getElementById('app'));
+})();
